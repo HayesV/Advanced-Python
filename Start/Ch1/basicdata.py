c030b4b1749e50b776a4d761450d39fc7415d75d
HayesV <92610413+HayesV@users.noreply.github.com>
2026-03-09 13:33:33 +0000

    first module

--- Start/Ch1/basicdata.py
+++ Start/Ch1/basicdata.py
@@ -9,10 +9,19 @@ with open("../../sample-weather-history.json", "r") as weatherfile:
     weatherdata = json.load(weatherfile)
 
 # TODO: What was the warmest day in the data set?
+#lambda function called over and over again on each element in the data
+warmDay = max(weatherdata, key=lambda x: x['tmax'])
+print(f"The warmest day was {warmDay['date']} at {warmDay['tmax']} degrees")
 
 
 # TODO: What was the coldest day in the data set?
 
+coldDay = min(weatherdata, key=lambda x: x['tmin'])
+print(f"The coldest day was {coldDay['date']} at {coldDay['tmin']} degrees")
 
 # TODO: How many days had snowfall?
+#list comprehension creates a new list in concise syntax
 
+snowDays = [day for day in weatherdata if day['snow'] > 0 ]
+print(f"Snow fell on {len(snowDays)} days")
+pprint.pp(snowDays)
